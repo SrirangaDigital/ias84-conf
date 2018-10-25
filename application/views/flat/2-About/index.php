bf2d70e569d4be460a20d79ad615293c60c6cc65
suresh07 <soori_rozee@yahoo.com>
2018-10-25 16:24:04 +0530

    Correction in About page updated

--- application/views/flat/2-About/index.php
+++ application/views/flat/2-About/index.php
@@ -11,12 +11,11 @@
         <div class="col-md-9 main">
             <h1>About</h1>
             <p>The 84th Annual Meeting of the Indian Academy of Sciences is being held at Varanasi, hosted by the Banaras Hindu University, during 2 – 4 November 2018. The two and a half days’ deliberation will see the participation of many Fellows and Associates of the Academy and invited teachers from different parts of the country who will engage in active scientific discussions with the Fellows.</p>
-            <p>The meeting will include inaugural lectures by newly elected Fellows and Associates over a wide range of scientific topics. The deliberations also include two special lectures on GM crops in the Indian context: Looking back, looking forward and The sciences of the artificial: Promise and reality,along with high-level scientific symposia, on Materials of Today and Tomorrow and Community Health– A Genomic Prospective. Encompassing wider areas, the 84th Annual Meeting will carry forward the tradition of public lectures by scholars from various fields. This year the public lectures include talks on Buddhism and science and the Changing faces of women in hindi cinema</p>
+            <p>The meeting will include inaugural lectures by newly elected Fellows and Associates over a wide range of scientific topics. The deliberations also include two special lectures on GM crops in the Indian context: Looking back, looking forward and The sciences of the artificial: Promise and reality,along with high-level scientific symposia, on Materials of Today and Tomorrow and Community Health– A Genomic Perspective. Encompassing wider areas, the 84th Annual Meeting will carry forward the tradition of public lectures by scholars from various fields. This year the public lectures include talks on Buddhism and science and the Changing faces of women in Hindi cinema</p>
             <p>The 84th Annual Meeting will also comprise many allied events including  the following:</p>
             <ul>
                 <li>IASC-ACS forum: Expanding Frontiers in Chemical Sciences</li>
                 <li>Advances in Earth System Sciences - A symposium funded by the Journal of Earth System Sciences</li>
-                <li>Teachers’ interaction session with Fellows and Associates of the Acadmey</li>
                 <li>Editorial Board meetings of Journal of Earth System Sciences and Pramana- Journal of Physics</li>
                 <li>Popular Science Workshop by tAcT, the Academy Trust</li>
                 <li>Exhibition of Academy’s publications (journals and books)</li>
