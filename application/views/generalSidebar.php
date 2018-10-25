bbad84b0d5c3adbfdc27187258bda399bad0b5ce
suresh07 <soori_rozee@yahoo.com>
2018-10-25 18:12:46 +0530

    Pdf files added

--- application/views/generalSidebar.php
+++ application/views/generalSidebar.php
@@ -22,5 +22,11 @@
     <h1>Downloads</h1>
     <ul>
         <li><a target="_blank" href="<?=DOWNLOAD_URL?>Abstract_Book.pdf">Speaker Profiles and Abstracts</a></li>
-        <li><a href="<?=DOWNLOAD_URL?>Program.pdf" target="_blank">Programme</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>Book_Release.pdf" target="_blank">Book Release</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>Cultural_Event.pdf" target="_blank">Cultural Event</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>Pocket_Book.pdf" target="_blank">Pocket Book</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>Programme_Poster.pdf" target="_blank">Programme</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>Public_Lectures.pdf" target="_blank">Public Lectures</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>Spl_Lectures_Poster.pdf" target="_blank">Special Lectures</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>Symposia_Poster_Final.pdf" target="_blank">Symposia</a></li>
     </ul>
