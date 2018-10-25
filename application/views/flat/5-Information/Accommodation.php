818e733c5ba179c6722dedf03740bb015e6cc44a
suresh07 <soori_rozee@yahoo.com>
2018-10-25 16:21:50 +0530

    Accommodation page updated and link given in home page

--- application/views/flat/5-Information/Accommodation.php
+++ application/views/flat/5-Information/Accommodation.php
@@ -11,13 +11,9 @@
         <div class="col-md-9 main">
             <h1>Hospitality</h1>
             <ul>
-                <li><strong>ACCOMODATION:</strong> The Academy has made arrangements to accommodate all of the meeting delegates. Details ​of accommodation will be provided at Gate-2 of the Infosys campus upon your arrival. Volunteers from the Academy will be at the Infosys Campus Gate to assist you with this.<br />For any queries regarding accommodation, please contact the <a href="<?=BASE_URL?>Information/Contact">Accommodation Committee</a>.</li><br />
-                <li><strong>RECEPTION AT MYSORE DEVELOPMENT CENTRE:</strong> The Secretariat of the Academy will be providing Identification Badges with names upon your arrival at the venue. The Identification Badge <strong>IS ESSENTIAL</strong> to get access to the Infosys campus via Gate no. 2. Please allow up to 15 minutes for the mandatory security screening. You have to wear the ID card inside the campus at all times.</li><br />
-                <li><strong>SECURITY COMPLIANCE:</strong> Please be aware that the Infosys Campus has high-security compliance and all delegates are subject to security inspection. We request you to inform us if you are carrying a laptop with you so that we can get prior approval.<br />For the duration of the meeting, all Academy delegates are permitted to enter and exit the Campus through the Gate 2 desk.</li><br />
-                <li><strong>MEDICAL SERVICES:</strong> A Walk-in Clinic is available on the Infosys campus during the meeting days.</li><br />
-                <li><strong>Wi-Fi FACILITY:</strong> The campus is facilitated with Wi-Fi facility and password keys will be provided.</li><br />
-                <li><strong>ATM FACILITY:</strong> ATM facility is available within the campus.</li>
-                <!-- <li>For any queries regarding accommodation, please contact the Accommodation <a href="<?=BASE_URL?>Information/Contact">Committee</a>.</li> -->
+                <li><strong>ACCOMODATION:</strong> The local organizing committee (LOC) has made arrangements to accommodate all of the meeting delegates. Stays have been arranged within and around the BHU campus. Details ​of accommodation will be provided upon your arrival. Volunteers from the LOC will be at the reception of Varanasi Airport to assist you with this.<br />For any queries regarding accommodation, please contact the <a href="<?=BASE_URL?>Information/Contact">Accommodation Committee</a></li><br />
+                <li><strong>MEDICAL SERVICES:</strong> with the help of LOC, medical services will be made available at the Health Centre of BHU during meeting days.<br />For any queries regarding accommodation, please contact the <a href="<?=BASE_URL?>Information/Contact">Medical Assistance subcommittee members</a></li><br />
+                <li><strong>Wi-Fi FACILITY:</strong> The meeting venue is Wi-Fi enabled, venue specific password may be obtained at the meeting venue. Internet connection with printer is facilitated at the venue.</li><br />
             </ul>             
         </div>
         <div class="col-md-3 sidebar">
