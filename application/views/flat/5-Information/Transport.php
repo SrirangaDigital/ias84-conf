afc8735ce4bacbdc3a159d30f3ed9aeeaff16c38
suresh07 <soori_rozee@yahoo.com>
2018-10-25 16:24:39 +0530

    Transport info updatex

--- application/views/flat/5-Information/Transport.php
+++ application/views/flat/5-Information/Transport.php
@@ -10,21 +10,9 @@
         <div class="row">
             <div class="col-md-9 main">
                 <h1>Logistics</h1>
-                <p>Transportation between the guest house and meeting venues will be provided. The Academy will provide pooled drop-off facility to participants to Bengaluru City and Bengaluru International Airport during and after the meeting. Participants are requested to provide departure details at the travel counter at the meeting venue.</p>
-                <h2>TRANSIT TO THE MEETING VENUE</h2>
-                <p>The Academy office has arranged pooled transportation from the following two points:</p>
-                <ul>
-                    <li><strong>Bangalore International Airport:</strong> Depending upon your travel itinerary, fly buses (delegates upon their arrival can directly proceed to Mysore by fly buses) and pooled cab services are arranged to travel further to Mysore. Individual-specific details will be provided to you by email. Academy staff Mr Vijaykumar (Mob.94482 46083) and Mr Jayakumar (Mob. 96327 26132) will assist you with the same, at the Airport.</li>
-                    <li><strong>Indian Academy of Sciences Campus:</strong> For the convenience of Bangalore-based Fellows and accompanied spouses who have registered for the meeting, transport facility is arranged on 28th June from the Academy. The journey will start at 2.00 pm. All registered Bangalore-based Fellows are requested to convene at the Academy by 1.30 pm.</li>
-                </ul>
-                <p>Registered participants who wish to arrive at the meeting venue on their own are requested to inform the Academy secretariat (mail to <execsec@ias.ac.in>) so that the access pass can be prepared.</p>
-                <p>It takes approximately 3–4 hours by road from Bengaluru city to the Infosys Campus in Mysuru.</p>
-                <h2>TRANSIT FROM THE MEETING VENUE</h2>
-                <p>On July 1, there transportation has been arranged from the Infosys Campus to Bengaluru and then to Bangalore Airport, after the breakfast, i.e., from 9.30 am onwards.  The secretariat of the Academy will facilitate the transit. Please note that one has to keep a 4-hours’ time window to board a flight from Bengaluru airport.</p>
-                <p>All registered Bangalore-based Fellows and accompanied spouses may please make use of this facility; they all will be dropped at the Academy premises in RRI campus, around noon.</p>
-                <h2>LOCAL TRANSPORTATION:</h2>
-                <p>Transportation services have been organized for all attendees during the meeting days in the Infosys campus to facilitate transit between the meeting venues and accommodation.</p>
-                <h2>TRAVEL FARE REIMBURSEMENTS:</h2>
+                <p>BHU campus is 30 km away from Varanasi airport and 6 km away from the railway station. Upon arrival, participants are requested to approach the reception desk at the airport for onward journey to the venue. This arrangement will be made as per the arrival details provided by the participants in the annual meeting link which had been provided to you by the Academy. For assistance you may contact the <a href="<?=BASE_URL?>Information/Contact">Transportation subcommittee</a> members</p>
+                <p>LOC will provide pooled drop-off facility to participants to airport during and after the meeting. Participants are requested to provide departure details at the travel counter at the meeting venue or update the travel plan in the annual meeting link provided to you. Please note that one has to keep practically two hours’ time window to board a flight from Varanasi airport./p>
+                <h2>Travel Fare Reimbursements:</h2>
                 <p>The Academy will extend travel fare reimbursement as per its norms. All participants are requested to fill in TA claim form available at the TA claim desk at the meeting venue. All claims must be supported with original pay receipts/invoice and boarding pass. The Academy will reimburse the travel claims over NEFT/RTGS.</p>
             </div>
             <div class="col-md-3 sidebar">
